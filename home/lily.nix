3ae02d59b9919445393142fd49551688afd8618c
Lillian Rose <me@lillianro.se>
2024-09-13 11:16:26 -0400

    fix: set some required session variables for sway. use vesktop from stable nixpkgs for now

--- home/lily.nix
+++ home/lily.nix
@@ -14,9 +14,10 @@
     username = "lily";
     homeDirectory = lib.mkDefault "/home/lily";
     packages = (with pkgs-unstable; [
-      vesktop
+      # vesktop until https://github.com/NixOS/nixpkgs/issues/340196 is solved somehow
     ]) ++ (with pkgs; [
       firefox-devedition-bin
+      vesktop
     ]);
 
     stateVersion = "24.05";
--- home/lily/sway.nix
+++ home/lily/sway.nix
@@ -39,6 +39,17 @@
     '';
   };
 
+  home.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+    "_JAVA_AWT_WM_NONREPARENTING" = "1";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION="1";
+    QT_QPA_PLATFORM="wayland";
+    SDL_VIDEODRIVER="wayland";
+    GDK_BACKEND="wayland";
+    XDG_SESSION_DESKTOP="sway";
+    MOZ_ENABLE_WAYLAND="1";
+  };
+
   home.file.".config/sway/bg.jpg" = {
     source = ../../assets/wallpaper.jpg;
   };
